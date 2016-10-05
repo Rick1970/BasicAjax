b6ece0a3ba65d0e2b48a9f981aaee7df8f70561a
rthor <rthor@LAPTOP-343NT6OO>
2016-10-05 11:23:54 -0700

    Basic ajax set up.

--- src/AjaxDemo/Startup.cs
+++ src/AjaxDemo/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AjaxDemo
 {
@@ -16,6 +17,7 @@ namespace AjaxDemo
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,10 +30,14 @@ namespace AjaxDemo
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=index}/{id?}");
             });
+
+           
         }
     }
 }
